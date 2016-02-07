f5a58fed0a595e107b8e5b92d6a28a75de76130c
Hossam <hossamShDesigns@gmail.com>
2016-02-08 00:59:58 +0200

    Music classes cleanup.

--- musicretriever/src/main/java/com/hussamsherif/musicretriever/MusicClasses/Album.java
+++ musicretriever/src/main/java/com/hussamsherif/musicretriever/MusicClasses/Album.java
@@ -1,61 +1,49 @@
 package com.hussamsherif.musicretriever.MusicClasses;
 
-import com.hussamsherif.musicretriever.AlbumArtColor;
-
 public class Album  {
-
-    public static final int IDENTIFIER = 1 ;
 
     private String name ;
     private String artistName;
     private long albumId;
     private long artistId ;
     private int numberOfSongs ;
-    private AlbumArtColor albumArtColor ;
 
-
-    public Album(String name, String artist ,long albumId, long artistId , int numberOfSongs , AlbumArtColor albumArtColor) {
+    public Album(String name, String artist ,long albumId, long artistId , int numberOfSongs) {
         this.name = name;
         this.artistName = artist ;
         this.albumId = albumId;
         this.artistId = artistId ;
         this.numberOfSongs = numberOfSongs;
-        this.albumArtColor = albumArtColor;
     }
 
+    /**
+     * @return The name of the album, if any
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * @return The unique id of the album
+     */
     public long getAlbumId() {
         return albumId;
     }
-
+    /**
+     * @return Number of songs contained in the album
+     */
     public int getNumberOfSongs() {
         return numberOfSongs;
     }
-
+    /**
+     * @return Artist Name who created the album
+     */
     public String getArtistName() {
         return artistName;
     }
-
+    /**
+     * @return The unique id of the artist who created the albuum
+     */
     public long getArtistId(){
         return artistId ;
-    }
-
-    public AlbumArtColor getAlbumArtColor() {
-        return albumArtColor;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-
-        if (!(o instanceof Album))
-            return false;
-
-        Album that = (Album) o ;
-        return this.albumId == that.albumId && this.name.matches(that.name) && this.artistId == that.artistId;
     }
 }
